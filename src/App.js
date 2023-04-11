e7c517b15db55c15be0ddc8a41841d0fda28ec8e
xenosign <xenosign@naver.com>
2023-04-11 17:21:25 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Show from './pages/Show';
 import Main from './pages/Main';
 import Login from './pages/Login';
 import KakaoRedirectHandler from './components/KakaoRedirectHandler';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   const isLogin = useSelector((state) => state.user.isLogin);
@@ -13,7 +14,6 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <Weather />
       <Routes>
         <Route path="/" element={isLogin ? <Main /> : <Login />} />
         <Route
