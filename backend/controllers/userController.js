d3ca496615591c34820a35bc6a1f9e2a52b4cadf
xenosign <xenosign@naver.com>
2023-04-12 09:28:40 +0900

    JWT 토큰 주석 추가

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -41,6 +41,7 @@ if (DB_MODE === 'mysql') {
     }
   };
 
+  // 토큰은 로그인 시 발행이 되므로, 여기에 모여 있습니다!
   const loginUser = (req, res) => {
     try {
       connection.query(
@@ -56,14 +57,19 @@ if (DB_MODE === 'mysql') {
             data[0].PASSWORD,
           );
 
+          // 비번이 동일하면 로그인이 성공 하였으므로, 토큰을 발행 합니다!
           if (isSamePassword) {
-            // accessToken 발행
+            // jwt 모듈을 사용하여 accessToken 발행
             const accessToken = jwt.sign(
-              { userID: data[0].USERID }, // 유저 정보
-              JWT_ACCESS_SECRET, // 일종의 salt
-              { expiresIn: '1d' }, // 옵션 중에서 만료기간
+              { userID: data[0].USERID }, // 유저 정보를 암호화하여 토큰 발행
+              JWT_ACCESS_SECRET, // 해당 JWT를 쉽게 풀 수 없도록, 암호키 삽입
+              { expiresIn: '1d' }, // 해당 토큰 만료기간 설정, 하루 동안 토큰을 인증할 수 있습니다
+              // 기간을 적게 하고 싶으면 ms 단위로 정하면 됩니다 (1000 * 60 -> 1분, 1000 * 60 * 60 -> 60분)
             );
 
+            // 생성 된 토큰을 프론트로 전달!
+            // 프론트에서는 로컬 스토리지에 저장 할 것이므로, 쿠키에 담지 않고 데이터로 전송
+            // 리액트 Login.jsx 에 가면 처리 코드가 있습니다!
             res
               .status(200)
               .json({ token: accessToken, message: '로그인 성공' });
@@ -96,9 +102,13 @@ if (DB_MODE === 'mysql') {
     }
   };
 
+  // 브라우저 로컬 스토리지에 저장 된, 토큰을 검증하는 컨트롤러
+  // 토큰 검증이 완료 되면 원하는 정보를 담아서 전달해 준다 -> 프론트에서는 로그인 처리
   const isToken = (req, res) => {
     jwt.verify(req.body.token, JWT_ACCESS_SECRET, (err, decoded) => {
+      // 토큰 검증 실패 시, 권한 없음 결과 전달
       if (err) return res.status(401).json('토큰 기한 만료');
+      // 토큰 검증 성공 시, 토큰을 푼 결과(decoded) 안의 userID 를 받아서 프론트에 전달
       return res
         .status(200)
         .json({ userID: decoded.userID, msg: '토큰 검증 완료' });
